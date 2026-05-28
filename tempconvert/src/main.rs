edf4df239a664fa31ad1918754e07144354a4312
bloodmoonfire <bluemoonfire87@gmail.com>
2026-05-27 19:02:50 -0500

    Edits to tempconvert

--- tempconvert/src/main.rs
+++ tempconvert/src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn main() {
     loop {
-    println!("Please enter a tempature: ");
+    println!("Please enter a tempature in fahrenheit: ");
         let mut temp = String::new();
 
         io::stdin()
@@ -13,10 +13,10 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        println!("{temp}");
+
         //(32°F − 32) × 5/9 = 0°C
-        let celsius = (temp -32.0) * (5.0/9.0);
-    println!("{celsius}");
+        let celsius = ((temp -32.0) * (5.0/9.0)).round();
+    println!("The tempature in celsius is: {celsius}");
     println!("Do you want to enter another temature?(Y/N)");
         let mut test = String::new();
 
@@ -24,7 +24,7 @@ fn main() {
             .read_line(&mut test)
             .expect("Failed to read line");
         let test: String = test.trim().to_lowercase();
-    println!("{test}");
+
     if test == "n" {
         break;
         }   
